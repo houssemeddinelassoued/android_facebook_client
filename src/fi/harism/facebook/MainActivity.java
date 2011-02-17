bb09238147d0b509cddd157b6d8230f96dd606d2
harism <harism@gmail.com>
2011-02-17 23:17:08 +0200

    Profile picture rounding added.

--- src/fi/harism/facebook/MainActivity.java
+++ src/fi/harism/facebook/MainActivity.java
@@ -7,7 +7,14 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -40,7 +47,27 @@ public class MainActivity extends Activity {
 					InputStream is = url.openStream();
 					Bitmap bitmap = BitmapFactory.decodeStream(is);
 
-					runnable.setNameBitmap(name, bitmap);
+					Bitmap roundedBitmap = Bitmap.createBitmap(
+							bitmap.getWidth(), bitmap.getHeight(),
+							Config.ARGB_8888);
+					Canvas canvas = new Canvas(roundedBitmap);
+
+					final int color = 0xff424242;
+					final Paint paint = new Paint();
+					final Rect rect = new Rect(0, 0, bitmap.getWidth(),
+							bitmap.getHeight());
+					final RectF rectF = new RectF(rect);
+					final float roundPx = 12;
+
+					paint.setAntiAlias(true);
+					canvas.drawARGB(0, 0, 0, 0);
+					paint.setColor(color);
+					canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+					paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+					canvas.drawBitmap(bitmap, rect, rect, paint);
+
+					runnable.setNameBitmap(name, roundedBitmap);
 					runOnUiThread(runnable);
 
 				} catch (Exception ex) {
