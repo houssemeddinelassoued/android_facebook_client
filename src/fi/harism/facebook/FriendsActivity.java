8be524516583202f29924358a4ddde464a5e3bf6
harism <harism@gmail.com>
2011-02-20 14:01:59 +0200

    Search functionality added to friends list.

--- src/fi/harism/facebook/FriendsActivity.java
+++ src/fi/harism/facebook/FriendsActivity.java
@@ -9,7 +9,12 @@ import org.json.JSONObject;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -28,6 +33,10 @@ public class FriendsActivity extends BaseActivity {
 		setContentView(R.layout.friends);
 
 		requestController = new RequestController(this);
+
+		SearchEditorObserver searchObserver = new SearchEditorObserver();
+		EditText searchEditor = (EditText) findViewById(R.id.friends_edit_search);
+		searchEditor.addTextChangedListener(searchObserver);
 
 		showProgressDialog();
 		getFriendsList();
@@ -117,6 +126,10 @@ public class FriendsActivity extends BaseActivity {
 				.findViewById(R.id.friends_item_image);
 		Bitmap scaled = BitmapUtils.scaleToHeight(imageRequest.getBitmap(), 30);
 		imageView.setImageBitmap(BitmapUtils.roundBitmap(scaled, 3));
+		
+		EditText searchEditText = (EditText)findViewById(R.id.friends_edit_search);
+		String searchText = searchEditText.getText().toString();
+		toggleFriendItemVisibility(friendView, searchText);
 
 		LinearLayout scrollView = (LinearLayout) findViewById(R.id.friends_list);
 		scrollView.addView(friendView);
@@ -129,6 +142,19 @@ public class FriendsActivity extends BaseActivity {
 				showAlertDialog((String) v.getTag(R.id.view_user_id));
 			}
 		});
+	}
+	
+	private final void toggleFriendItemVisibility(View friendItem, String searchText) {
+		searchText = searchText.toLowerCase();
+		TextView friendTextView = (TextView)friendItem.findViewById(R.id.friends_item_name);
+		String friendName = friendTextView.getText().toString();
+		friendName = friendName.toLowerCase();
+		
+		if (friendName.contains(searchText)) {
+			friendItem.setVisibility(View.VISIBLE);
+		} else {
+			friendItem.setVisibility(View.GONE);
+		}
 	}
 
 	private final class FacebookMeFriendsObserver implements
@@ -157,6 +183,28 @@ public class FriendsActivity extends BaseActivity {
 		@Override
 		public void onComplete(ImageRequest imageRequest) {
 			onImageReceived(imageRequest);
+		}
+	}
+
+	private final class SearchEditorObserver implements TextWatcher {
+		@Override
+		public void afterTextChanged(Editable editable) {
+			String searchText = editable.toString();
+			ViewGroup friendList = (LinearLayout)findViewById(R.id.friends_list);
+			for (int i=0; i<friendList.getChildCount(); ++i) {
+				LinearLayout friendItem = (LinearLayout)friendList.getChildAt(i);
+				toggleFriendItemVisibility(friendItem, searchText);
+			}
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+				int arg3) {
+		}
+
+		@Override
+		public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+				int arg3) {
 		}
 	}
 
