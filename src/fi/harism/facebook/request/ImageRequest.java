b98b85b88d59f7dd44ed3409caf4bfe44d97f194
harism <harism@gmail.com>
2011-02-21 22:44:12 +0200

    Comments added.

--- src/fi/harism/facebook/request/ImageRequest.java
+++ src/fi/harism/facebook/request/ImageRequest.java
@@ -7,19 +7,46 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+/**
+ * ImageRequest class for loading images asynchronously.
+ * 
+ * @author harism
+ */
 public class ImageRequest extends Request {
 
+	// Image URL.
 	private String url;
-	private Observer observer;
+	// Observer for ImageRequest.
+	private ImageRequest.Observer observer;
+	// Bitmap we loaded.
 	private Bitmap bitmap;
 
+	/**
+	 * Constructor for ImageRequest.
+	 * 
+	 * @param activity
+	 *            Activity to which use for runOnUiThread.
+	 * @param requestObserver
+	 *            Observer for Request base class.
+	 * @param url
+	 *            Image URL.
+	 * @param observer
+	 *            ImageRequest observer.
+	 */
 	public ImageRequest(Activity activity, Request.Observer requestObserver,
-			String url, Observer observer) {
+			String url, ImageRequest.Observer observer) {
 		super(activity, requestObserver);
 		this.url = url;
 		this.observer = observer;
+		bitmap = null;
 	}
 
+	/**
+	 * Once ImageRequest is completed successfully this method returns loaded
+	 * Bitmap.
+	 * 
+	 * @return Loaded Bitmap or null on error.
+	 */
 	public Bitmap getBitmap() {
 		return bitmap;
 	}
@@ -41,10 +68,24 @@ public class ImageRequest extends Request {
 		observer.onComplete(this);
 	}
 
+	/**
+	 * ImageRequest observer.
+	 */
 	public interface Observer {
+		/**
+		 * Called once ImageRequest is done successfully.
+		 * 
+		 * @param imageRequest
+		 *            ImageRequest object being completed.
+		 */
 		public void onComplete(ImageRequest imageRequest);
 
+		/**
+		 * Called if ImageRequest failed.
+		 * 
+		 * @param ex
+		 *            Exception explaining the cause for failure.
+		 */
 		public void onError(Exception ex);
 	}
-
 }
