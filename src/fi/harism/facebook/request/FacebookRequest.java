a87b093c935a075448c06db54dd29872834e79c8
harism <harism@gmail.com>
2011-02-21 22:43:35 +0200

    Comments added.

--- src/fi/harism/facebook/request/FacebookRequest.java
+++ src/fi/harism/facebook/request/FacebookRequest.java
@@ -6,32 +6,56 @@ import android.os.Bundle;
 import fi.harism.facebook.BaseActivity;
 import fi.harism.facebook.util.FacebookController;
 
+/**
+ * FacebookRequest class is implemented for making asynchronous Facebook Graph
+ * API calls.
+ * 
+ * @author harism
+ */
 public class FacebookRequest extends Request {
 
+	// Facebook Graph API path.
 	private String requestPath;
+	// Facebook Graph API parameters.
 	private Bundle requestBundle;
-	private Observer observer;
+	// Observer.
+	private FacebookRequest.Observer observer;
+	// Response from server.
 	private JSONObject response;
+	// We need Activity to retrieve FacebookController instance.
 	private BaseActivity baseActivity;
 
-	public FacebookRequest(BaseActivity activity, Request.Observer requestObserver,
-			String requestPath, Bundle requestBundle, Observer observer) {
+	/**
+	 * Constructor for Facebook request.
+	 * 
+	 * @param activity
+	 *            Activity needed for
+	 * @param requestObserver
+	 *            Request observer for base class.
+	 * @param requestPath
+	 *            Facebook Graph API path.
+	 * @param requestBundle
+	 *            Facebook Graph API parameters.
+	 * @param observer
+	 *            Facebook request observer.
+	 */
+	public FacebookRequest(BaseActivity activity,
+			Request.Observer requestObserver, String requestPath,
+			Bundle requestBundle, FacebookRequest.Observer observer) {
 		super(activity, requestObserver);
 		this.requestPath = requestPath;
 		this.requestBundle = requestBundle;
 		this.observer = observer;
 		this.baseActivity = activity;
+		response = null;
 	}
 
-	public FacebookRequest(BaseActivity activity, Request.Observer requestObserver,
-			String requestPath, Observer observer) {
-		super(activity, requestObserver);
-		this.requestPath = requestPath;
-		this.requestBundle = null;
-		this.observer = observer;
-		this.baseActivity = activity;
-	}
-
+	/**
+	 * Returns response from the server as JSONObject if this request has been
+	 * completed successfully, null otherwise.
+	 * 
+	 * @return Response we received from the server.
+	 */
 	public JSONObject getJSONObject() {
 		return response;
 	}
@@ -40,15 +64,20 @@ public class FacebookRequest extends Request {
 	public void runOnThread() throws Exception {
 		try {
 			String r;
-			FacebookController c = baseActivity.getGlobalState().getFacebookController();
+			// Get application wide instance of FacebookController.
+			FacebookController c = baseActivity.getGlobalState()
+					.getFacebookController();
+			// Make actual request.
 			if (requestBundle != null) {
 				r = c.request(requestPath, requestBundle);
 			} else {
 				r = c.request(requestPath);
 			}
 
+			// Create JSONObject from response string.
 			response = new JSONObject(r);
 
+			// Check if response is an error JSONObject.
 			if (response.has("error")) {
 				JSONObject err = response.getJSONObject("error");
 				Exception ex = new Exception(err.getString("type") + " : "
@@ -66,10 +95,24 @@ public class FacebookRequest extends Request {
 		observer.onComplete(this);
 	}
 
+	/**
+	 * Interface for listening to FacebookRequest.
+	 */
 	public interface Observer {
+		/**
+		 * Called once FacebookRequest finishes successfully.
+		 * 
+		 * @param facebookRequest
+		 *            FacebookRequest that was completed.
+		 */
 		public void onComplete(FacebookRequest facebookRequest);
 
+		/**
+		 * Called once FacebookRequest fails.
+		 * 
+		 * @param ex
+		 *            Exception containing reason for error.
+		 */
 		public void onError(Exception ex);
 	}
-
 }
