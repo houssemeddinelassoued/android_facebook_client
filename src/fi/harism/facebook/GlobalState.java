e581df4c130b5c004e48deec6aa93f4c0bd0e854
harism <harism@gmail.com>
2011-03-11 20:20:27 +0200

    GlobalState divided into GlobalState interface and GlobalStateImpl implementation.

--- src/fi/harism/facebook/GlobalState.java
+++ src/fi/harism/facebook/GlobalState.java
@@ -1,25 +1,15 @@
 package fi.harism.facebook;
 
-import android.app.Application;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import fi.harism.facebook.util.DataCache;
 import fi.harism.facebook.util.FacebookController;
 
 /**
- * GlobalState class extends Application and is used as base class for our
- * application. It is used for storing application wide data among Activities.
+ * GlobalState interface.
  * 
  * @author harism
  */
-public class GlobalState extends Application {
-
-	// Instance of FacebookController.
-	private FacebookController facebookController = null;
-	// Instance of DataCache.
-	private DataCache dataCache = null;
-	// Default profile picture.
-	private Bitmap defaultPicture = null;
+public interface GlobalState {
 
 	/**
 	 * Returns application wide instance of DataCache. Creates one once this
@@ -27,12 +17,7 @@ public class GlobalState extends Application {
 	 * 
 	 * @return DataCache instance.
 	 */
-	public DataCache getDataCache() {
-		if (dataCache == null) {
-			dataCache = new DataCache();
-		}
-		return dataCache;
-	}
+	public DataCache getDataCache();
 
 	/**
 	 * Returns instance of default profile picture. Creates one once this method
@@ -40,13 +25,7 @@ public class GlobalState extends Application {
 	 * 
 	 * @return Default profile picture Bitmap.
 	 */
-	public Bitmap getDefaultPicture() {
-		if (defaultPicture == null) {
-			defaultPicture = BitmapFactory.decodeResource(getResources(),
-					R.drawable.picture_default);
-		}
-		return defaultPicture;
-	}
+	public Bitmap getDefaultPicture();
 
 	/**
 	 * Returns application wide instance of FacebookController. Creates one once
@@ -54,11 +33,6 @@ public class GlobalState extends Application {
 	 * 
 	 * @return FacebookController instance.
 	 */
-	public FacebookController getFacebookController() {
-		if (facebookController == null) {
-			facebookController = new FacebookController();
-		}
-		return facebookController;
-	}
+	public FacebookController getFacebookController();
 
 }
---
+++ src/fi/harism/facebook/GlobalStateImpl.java
@@ -0,0 +1,49 @@
+package fi.harism.facebook;
+
+import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import fi.harism.facebook.util.DataCache;
+import fi.harism.facebook.util.FacebookController;
+
+/**
+ * GlobalStateImpl class extends Application and is used as base class for our
+ * application. It is used for storing application wide data among Activities.
+ * 
+ * @author harism
+ */
+public class GlobalStateImpl extends Application implements GlobalState {
+
+	// Instance of FacebookController.
+	private FacebookController facebookController = null;
+	// Instance of DataCache.
+	private DataCache dataCache = null;
+	// Default profile picture.
+	private Bitmap defaultPicture = null;
+
+	@Override
+	public DataCache getDataCache() {
+		if (dataCache == null) {
+			dataCache = new DataCache();
+		}
+		return dataCache;
+	}
+
+	@Override
+	public Bitmap getDefaultPicture() {
+		if (defaultPicture == null) {
+			defaultPicture = BitmapFactory.decodeResource(getResources(),
+					R.drawable.picture_default);
+		}
+		return defaultPicture;
+	}
+
+	@Override
+	public FacebookController getFacebookController() {
+		if (facebookController == null) {
+			facebookController = new FacebookController();
+		}
+		return facebookController;
+	}
+
+}
